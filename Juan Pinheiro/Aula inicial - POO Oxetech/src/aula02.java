2ed03afad7905eb956a21632ae8dad7f26399272
Juan Rodrigo de Oliveira Pinheiro <101989093+juanpinheirx@users.noreply.github.com>
2024-01-10 20:17:38 -0300

    aula02: exercícios com o método Scanner(System.in)

---
+++ Juan Pinheiro/Aula inicial - POO Oxetech/src/aula02.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class aula02 {
+    public static void main(String[] args) {
+//        // desvios de condição. (if...else, else if)
+//        double nota1, nota2, nota3, nota4;
+//        nota1 = 10; //instrução
+//        nota2 = 9;
+//        nota3 = 8.8;
+//        nota4 = 10;
+
+////        exercício 1
+//        Scanner input = new Scanner(System.in); // Scanner = classe responsável por guardar dados
+//        String nameInput = input.nextLine(); // método nativo do Scanner: pergunta no terminal o valor pedido.
+//        System.out.println("O nome guardado foi: " + nameInput);
+
+
+////        exercício 2
+//        Scanner input2 = new Scanner(System.in);
+//        Scanner input3 = new Scanner(System.in);
+//        Number number = input2.nextInt();
+//        Number number1 = input3.nextInt();
+//        Number resultSum = number1.intValue() + number.intValue();
+//        Number resultProd = number1.intValue() * number.intValue();
+//        Number resultAvg = (number1.intValue() + number.intValue()) / 2;
+//        Number resultRaz = number1.intValue() / number.intValue();
+//        Number resultRest = number1.intValue() % number.intValue();
+//        System.out.println("A soma é: " + resultSum + "\n"
+//                + "O produto é: " + resultProd + "\n"
+//                + "A média é: " + resultAvg + "\n"
+//                + "A razão é: " + resultRaz + "\n"
+//                + "O resto é: " + resultRest);
+////        exercício 3 refatorado
+//        Scanner inputNum = new Scanner(System.in);
+//        int num1 = inputNum.nextInt();
+//        int num2 = inputNum.nextInt();
+//        System.out.println("A soma dos números é: " + (num1 + num2));
+//        System.out.println("O produto dos valores é: " + (num1 * num2));
+//        System.out.println("A média dos valores é: " + ((num1 + num2) / 2));
+//        System.out.println("A razão dos valores é: " + (num1 / num2));
+//        System.out.println("O resto dos valores é: " + (num1 & num2));
+////        exercício 3
+//        Scanner inputNum = new Scanner(System.in);
+//        int num = inputNum.nextInt(); // prestar atenção na hora de tipar as variáveis
+//        if (num % 2 == 0) {
+//            System.out.println("O número é par.");
+//        } else {
+//            System.out.println("O número é ímpar.");
+//        }
+////        exercício 4
+//        Scanner inputNum = new Scanner(System.in);
+//        Double num1 = inputNum.nextDouble();
+//        Double num2 = inputNum.nextDouble();
+//        Double num3 = inputNum.nextDouble();
+//        Double result = (num1 + num2 + num3) / 3;
+//        System.out.println("A média dos valores é: " + result);
+    }
+}
